03b48d6240041838abe6c224c4a9dc2c40804abf
Krešimir Forjan <kresimirforjan5@gmail.com>
2024-08-19 18:20:58 +0200

    Add freezed support

--- lua/flutter-bloc/init.lua
+++ lua/flutter-bloc/init.lua
@@ -4,121 +4,143 @@ local bloc_template = require("flutter-bloc.templates")
 local M = {}
 
 M.setup = function(opts)
+	-- Store the options, specifically the 'freezed' option
+	M.opts = opts or {}
+	M.opts.freezed = M.opts.freezed or false
 end
 
 M.create_cubit = function()
-    local cubit_name = vim.fn.input("Cubit name: ")
-    if cubit_name == "" then
-        print('The cubit name must not be empty')
-        return
-    end
-    local snake_case_cubit_name = util.camel_to_snake(cubit_name)
+	local cubit_name = vim.fn.input("Cubit name: ")
+	if cubit_name == "" then
+		print("The cubit name must not be empty")
+		return
+	end
+	local snake_case_cubit_name = util.camel_to_snake(cubit_name)
 
-    local buf_directory = util.get_current_buffer_path()
-    buf_directory = buf_directory .. "cubit/" .. snake_case_cubit_name .. "_cubit/"
+	local buf_directory = util.get_current_buffer_path()
+	buf_directory = buf_directory .. "cubit/" .. snake_case_cubit_name .. "_cubit/"
 
-    -- Get bloc path from user
-    local cubit_path = vim.fn.input("Cubit path: ", buf_directory)
-    if cubit_path == "" then
-        print('The Cubit path must not be empty')
-        return
-    end
+	-- Get bloc path from user
+	local cubit_path = vim.fn.input("Cubit path: ", buf_directory)
+	if cubit_path == "" then
+		print("The Cubit path must not be empty")
+		return
+	end
 
-    -- Create a directory if it doesn't exist
-    vim.fn.mkdir(cubit_path, "p")
+	-- Create a directory if it doesn't exist
+	vim.fn.mkdir(cubit_path, "p")
 
-    local files = {
-        snake_case_cubit_name .. "_cubit.dart",
-        snake_case_cubit_name .. "_state.dart"
-    }
+	local files = {
+		snake_case_cubit_name .. "_cubit.dart",
+		snake_case_cubit_name .. "_state.dart",
+	}
 
-    local templates = {
-        bloc_template.create_cubit_template(cubit_name),
-        bloc_template.create_cubit_state_template(cubit_name),
-    }
+	local templates = {
+		bloc_template.create_cubit_template(cubit_name),
+		bloc_template.create_cubit_state_template(cubit_name),
+	}
 
-    -- Write templates to files
-    for i, file in ipairs(files) do
-        local file_full_path = cubit_path .. file
-        local f = io.open(file_full_path, "w")
-        f:write(templates[i])
-        f:close()
-        vim.cmd("edit " .. file_full_path)
-    end
+	-- Write templates to files
+	for i, file in ipairs(files) do
+		local file_full_path = cubit_path .. file
+		local f = io.open(file_full_path, "w")
+		f:write(templates[i])
+		f:close()
+		vim.cmd("edit " .. file_full_path)
+	end
 
-    vim.cmd("edit " .. cubit_path .. files[1])
+	vim.cmd("edit " .. cubit_path .. files[1])
 end
 
 M.create_bloc = function()
-    local bloc_name = vim.fn.input("Bloc name: ")
-    if bloc_name == "" then
-        print('The bloc name must not be empty')
-        return
-    end
-    local snake_case_bloc_name = util.camel_to_snake(bloc_name)
+	local bloc_name = vim.fn.input("Bloc name: ")
+	if bloc_name == "" then
+		print("The bloc name must not be empty")
+		return
+	end
+	local snake_case_bloc_name = util.camel_to_snake(bloc_name)
 
-    -- Get the current buffer's path
-    local buf_directory = util.get_current_buffer_path()
-    buf_directory = buf_directory .. "bloc/" .. snake_case_bloc_name .. "_bloc/"
+	-- Get the current buffer's path
+	local buf_directory = util.get_current_buffer_path()
+	buf_directory = buf_directory .. "bloc/" .. snake_case_bloc_name .. "_bloc/"
 
-    -- Get bloc path from user
-    local bloc_path = vim.fn.input("Bloc path: ", buf_directory)
-    if bloc_path == "" then
-        print('The bloc path must not be empty')
-        return
-    end
+	-- Get bloc path from user
+	local bloc_path = vim.fn.input("Bloc path: ", buf_directory)
+	if bloc_path == "" then
+		print("The bloc path must not be empty")
+		return
+	end
 
-    -- Create a directory if it doesn't exist
-    vim.fn.mkdir(bloc_path, "p")
+	-- Create a directory if it doesn't exist
+	vim.fn.mkdir(bloc_path, "p")
 
-    local files = {
-        snake_case_bloc_name .. "_bloc.dart",
-        snake_case_bloc_name .. "_event.dart",
-        snake_case_bloc_name .. "_state.dart"
-    }
+	local templates
+	if M.opts.freezed then
+		templates = {
+			bloc_template.create_bloc_template_freezed(bloc_name),
+			bloc_template.create_event_template_freezed(bloc_name),
+			bloc_template.create_bloc_state_template_freezed(bloc_name),
+		}
+	else
+		templates = {
+			bloc_template.create_bloc_template_equatable(bloc_name),
+			bloc_template.create_event_template_equatable(bloc_name),
+			bloc_template.create_bloc_state_template_equatable(bloc_name),
+		}
+	end
 
-    local templates = {
-        bloc_template.create_bloc_template(bloc_name),
-        bloc_template.create_event_template(bloc_name),
-        bloc_template.create_bloc_state_template(bloc_name),
-    }
+	local files = {
+		snake_case_bloc_name .. "_bloc.dart",
+		snake_case_bloc_name .. "_event.dart",
+		snake_case_bloc_name .. "_state.dart",
+	}
 
-    -- Write templates to files
-    for i, file in ipairs(files) do
-        local file_full_path = bloc_path .. file
-        local f = io.open(file_full_path, "w")
-        f:write(templates[i])
-        f:close()
-    end
-    vim.cmd("edit " .. bloc_path .. files[1])
+	-- Write templates to files
+	for i, file in ipairs(files) do
+		local file_full_path = bloc_path .. file
+		local f = io.open(file_full_path, "w")
+		f:write(templates[i])
+		f:close()
+	end
+	vim.cmd("edit " .. bloc_path .. files[1])
 end
 
 M.create_cubit_template = function(cubit_name)
-    local snake_case_cubit_name = util.camel_to_snake(cubit_name)
+	local snake_case_cubit_name = util.camel_to_snake(cubit_name)
 
-    local template = {
-        cubit = bloc_template.create_cubit_template(cubit_name),
-        state = bloc_template.create_cubit_state_template(cubit_name),
-        cubit_name = snake_case_cubit_name .. "_cubit.dart",
-        state_name = snake_case_cubit_name .. "_state.dart"
-    }
+	local template = {
+		cubit = bloc_template.create_cubit_template(cubit_name),
+		state = bloc_template.create_cubit_state_template(cubit_name),
+		cubit_name = snake_case_cubit_name .. "_cubit.dart",
+		state_name = snake_case_cubit_name .. "_state.dart",
+	}
 
-    return template
+	return template
 end
 
 M.create_bloc_template = function(bloc_name)
-    local snake_case_bloc_name = util.camel_to_snake(bloc_name)
+	local snake_case_bloc_name = util.camel_to_snake(bloc_name)
 
-    local template = {
-        bloc = bloc_template.create_bloc_template(bloc_name),
-        state = bloc_template.create_bloc_state_template(bloc_name),
-        event = bloc_template.create_event_template(bloc_name),
-        bloc_name = snake_case_bloc_name .. "_bloc.dart",
-        state_name = snake_case_bloc_name .. "_state.dart",
-        event_name = snake_case_bloc_name .. "_event.dart"
-    }
+	local template
+	if M.opts.freezed then
+		template = {
+			bloc = bloc_template.create_bloc_template_freezed(bloc_name),
+			state = bloc_template.create_bloc_state_template_freezed(bloc_name),
+			event = bloc_template.create_event_template_freezed(bloc_name),
+		}
+	else
+		template = {
+			bloc = bloc_template.create_bloc_template_equatable(bloc_name),
+			state = bloc_template.create_bloc_state_template_equatable(bloc_name),
+			event = bloc_template.create_event_template_equatable(bloc_name),
+		}
+	end
 
-    return template
+	template.bloc_name = snake_case_bloc_name .. "_bloc.dart"
+	template.state_name = snake_case_bloc_name .. "_state.dart"
+	template.event_name = snake_case_bloc_name .. "_event.dart"
+
+	return template
 end
 
 return M
--- lua/flutter-bloc/templates/init.lua
+++ lua/flutter-bloc/templates/init.lua
@@ -1,11 +1,11 @@
 local bloc = {}
 local util = require("flutter-bloc.util")
 
-bloc.create_bloc_template = function(bloc_name)
-    local snake_case_name = util.camel_to_snake(bloc_name)
+bloc.create_bloc_template_equatable = function(bloc_name)
+	local snake_case_name = util.camel_to_snake(bloc_name)
 
-    local template = string.format(
-        [[
+	local template = string.format(
+		[[
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -20,24 +20,24 @@ class %sBloc extends Bloc<%sEvent, %sState> {
   }
 }
 ]],
-        snake_case_name,
-        snake_case_name,
-        bloc_name,
-        bloc_name,
-        bloc_name,
-        bloc_name,
-        bloc_name,
-        bloc_name
-    )
+		snake_case_name,
+		snake_case_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name
+	)
 
-    return template
+	return template
 end
 
-bloc.create_event_template = function(bloc_name)
-    local snake_case_name = util.camel_to_snake(bloc_name)
+bloc.create_event_template_equatable = function(bloc_name)
+	local snake_case_name = util.camel_to_snake(bloc_name)
 
-    local template = string.format(
-        [[
+	local template = string.format(
+		[[
 part of '%s_bloc.dart';
 
 sealed class %sEvent extends Equatable {
@@ -47,19 +47,19 @@ sealed class %sEvent extends Equatable {
   List<Object> get props => [];
 }
 ]],
-        snake_case_name,
-        bloc_name,
-        bloc_name
-    )
+		snake_case_name,
+		bloc_name,
+		bloc_name
+	)
 
-    return template
+	return template
 end
 
-bloc.create_bloc_state_template = function(bloc_name)
-    local snake_case_name = util.camel_to_snake(bloc_name)
+bloc.create_bloc_state_template_equatable = function(bloc_name)
+	local snake_case_name = util.camel_to_snake(bloc_name)
 
-    local template = string.format(
-        [[
+	local template = string.format(
+		[[
 part of '%s_bloc.dart';
 
 sealed class %sState extends Equatable {
@@ -71,70 +71,151 @@ sealed class %sState extends Equatable {
 
 final class %sInitial extends %sState {}
 ]],
-        snake_case_name,
-        bloc_name,
-        bloc_name,
-        bloc_name,
-        bloc_name
-    )
+		snake_case_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name
+	)
 
-    return template
+	return template
 end
 
+bloc.create_bloc_template_freezed = function(bloc_name)
+	local snake_case_name = util.camel_to_snake(bloc_name)
+
+	local template = string.format(
+		[[
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '%s_event.dart';
+part '%s_state.dart';
+part '%s_bloc.freezed.dart'
+
+class %sBloc extends Bloc<%sEvent, %sState> {
+  %sBloc() : super(_Initial()) {
+    on<%sEvent>(_on%sEvent);
+  }
+
+  void _on%sEvent(
+    %sEvent event,
+    Emitter<%sState> emit,
+  ){
+      event.map(
+        initial: (event) { //TODO(): Implement event handler },
+      );
+  }
+}
+]],
+		snake_case_name,
+		snake_case_name,
+		snake_case_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name
+	)
+
+	return template
+end
+
+bloc.create_event_template_freezed = function(bloc_name)
+	local snake_case_name = util.camel_to_snake(bloc_name)
+
+	local template = string.format(
+		[[
+part of '%s_bloc.dart';
+
+@freezed
+class %sEvent with _$%sEvent {
+  const factory %sEvent.started() = _Started;
+}
+
+]],
+		snake_case_name,
+		bloc_name,
+		bloc_name,
+		bloc_name
+	)
+
+	return template
+end
+
+bloc.create_bloc_state_template_freezed = function(bloc_name)
+	local snake_case_name = util.camel_to_snake(bloc_name)
+
+	local template = string.format(
+		[[
+part of '%s_bloc.dart';
+
+@freezed
+class %sState extends _$%sState {
+  const factory %sState.initial() = _Initial;
+}
+
+]],
+		snake_case_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name
+	)
+
+	return template
+end
 
 -- Cubit templates
 bloc.create_cubit_template = function(bloc_name)
-    local snake_case_name = util.camel_to_snake(bloc_name)
+	local snake_case_name = util.camel_to_snake(bloc_name)
 
-    local template = string.format(
-        [[
-import 'package:equatable/equatable.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+	local template = string.format(
+		[[
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
 
 part '%s_state.dart';
 
 class %sCubit extends Cubit<%sState> {
   %sCubit() : super(%sInitial());
 }
-]],
-        snake_case_name,
-        bloc_name,
-        bloc_name,
-        bloc_name,
-        bloc_name,
-        bloc_name,
-        bloc_name,
-        bloc_name
-    )
 
-    return template
+]],
+		snake_case_name,
+		bloc_name,
+		bloc_name,
+		bloc_name,
+		bloc_name
+	)
+
+	return template
 end
 
 bloc.create_cubit_state_template = function(bloc_name)
-    local snake_case_name = util.camel_to_snake(bloc_name)
+	local snake_case_name = util.camel_to_snake(bloc_name)
 
-    local template = string.format(
-        [[
+	local template = string.format(
+		[[
 part of '%s_cubit.dart';
 
-sealed class %sState extends Equatable {
-  const %sState();
-
-  @override
-  List<Object> get props => [];
-}
+@immutable
+sealed class %sState {}
 
 final class %sInitial extends %sState {}
+
 ]],
-        snake_case_name,
-        bloc_name,
-        bloc_name,
-        bloc_name,
-        bloc_name
-    )
+		snake_case_name,
+		bloc_name,
+		bloc_name,
+		bloc_name
+	)
 
-    return template
+	return template
 end
-
 
 return bloc
